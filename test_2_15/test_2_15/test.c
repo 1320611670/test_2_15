dd8b30fa36898d7ef786f997f0a7cba36777d2c5
一叶之秋 <1320611670@qq.com>
2021-02-15 16:38:40 +0800

    c语言实用调试技巧

---
+++ test_2_15/test_2_15/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i = 0;
+	for (i = 0; i < 100; i++)
+	{
+		printf("%d ", i);
+	}
+	system("pause");
+	return 0;
+}
